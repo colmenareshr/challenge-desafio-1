87978559e972bbcdfa049269e1f99e9d74272b3c
Humberto Colmenares <colmenareshr@gmail.com>
2022-12-26 20:40:07 -0300

    add encrypting

---
+++ assets/js/app.js
@@ -0,0 +1,61 @@
+const textArea = document.getElementById('input-text');
+const outputDiv = document.getElementById('output-text');
+const encryptButton = document.getElementById('encrypt-button');
+const decryptButton = document.getElementById('decrypt-button');
+
+textArea.addEventListener('input', () => {
+  let inputText = textArea.value;
+  //We validate that there are no special Mayustulas and characters
+  const regex = /^[a-z ,.]*$/;
+  const isValid = textArea.value.match(regex);
+  if (!isValid) {
+    inputText = inputText.replace(/[^a-z]/g, '');
+  }
+  //We are it for all the characters of the input
+});
+
+const mapping = {
+  e: 'enter',
+  i: 'imes',
+  a: 'ai',
+  o: 'ober',
+  u: 'ufat',
+};
+
+function encrypting(text) {
+  let encryptedText = '';
+
+  for (let i = 0; i < text.length; i++) {
+    const character = text[i];
+    encryptedText += mapping[character] || character;
+  }
+  return encryptedText;
+}
+
+function decrypting(text) {
+  let decryptedText = '';
+
+  for (let i = 0; i < text.length; i++) {
+    const character = text[i];
+    if(character in mapping) {
+      decryptedText += character;
+      i += map
+    }
+  }
+  return decryptedText;
+}
+
+encryptButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  let encryptText = encrypting(textArea.value);
+  outputDiv.textContent = encryptText;
+  textArea.value = '';
+});
+
+decryptButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  let decryptText = decrypting(textArea.value);
+  outputDiv.textContent = '';
+  outputDiv.textContent = decryptText;
+  textArea.value = '';
+});
