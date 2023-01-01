72061117e5a99b35ede59ded342c8368633f7d89
Humberto Colmenares <colmenareshr@gmail.com>
2023-01-01 11:56:32 -0300

    add mapping and inverse Mappring

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
 function validateInput() {
   const textArea = document.getElementById('input-text');
   textArea.addEventListener('input', () => {
-    // Validamos que no haya mayúsculas o caracteres especiales
+    // We validate that there are no capital letters or special characters
     const regex = /^[a-z ,.]*$/;
     const isValid = textArea.value.match(regex);
     if (!isValid) {
@@ -24,3 +24,16 @@ function validateInput() {
 
 // We call the function when the page is loaded
 document.addEventListener('DOMContentLoaded', validateInput);
+
+const mapping = {
+  e: 'enter',
+  i: 'imes',
+  a: 'ai',
+  o: 'ober',
+  u: 'ufat',
+};
+
+const inverseMapping = {};
+for (const [key, value] of Object.entries(mapping)) {
+  inverseMapping[value] = key;
+}
