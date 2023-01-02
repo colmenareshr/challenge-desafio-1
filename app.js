7e80c376a9c3745d76a4136338aa239140593862
Humberto Colmenares <colmenareshr@gmail.com>
2023-01-01 22:15:46 -0300

    add decrypt fuction

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const textArea = document.getElementById('input-text');
 const outputArea = document.getElementById('result-text');
 const encryptButton = document.getElementById('encrypt-button');
 const decryptButton = document.getElementById('decrypt-button');
-const backgroundImage = document.getElementById('background-img');
 const copyBtn = document.getElementById('copy-btn');
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -10,21 +9,15 @@ document.addEventListener('DOMContentLoaded', function () {
   textarea.focus();
 });
 
-function validateInput() {
-  textArea.addEventListener('input', () => {
-    // We validate that there are no capital letters or special characters
-    const regex = /^[a-z ,.]*$/;
-    const isValid = textArea.value.match(regex);
-    if (!isValid) {
-      textArea.value = textArea.value.replace(/[^a-z ]/g, '');
-    }
-  });
-}
+textArea.addEventListener('input', () => {
+  //We validate that there are no special Mayustulas and characters
+  const regex = /^[a-z ,.]*$/;
+  const isValid = textArea.value.match(regex);
+  if (!isValid) {
+    textArea.value = textArea.value.replace(/[^a-z ]/g, '');
+  }
+});
 
-// We call the function when the page is loaded
-document.addEventListener('DOMContentLoaded', validateInput);
-
-// This object contains the character mapping to encrypt
 const mapping = {
   e: 'enter',
   i: 'imes',
@@ -33,27 +26,35 @@ const mapping = {
   u: 'ufat',
 };
 
-// We create an inverse mapping to facilitate the deciphering
-const inverseMapping = {};
-for (const [key, value] of Object.entries(mapping)) {
-  inverseMapping[value] = key;
-}
+function encrypting(text, mapping) {
+  let encryptedText = '';
 
-// This function transforms a text using the specified mapping
-function transformText(text, mapping) {
-  let transformedText = '';
   for (let i = 0; i < text.length; i++) {
     const character = text[i];
-    transformedText += mapping[character] || character;
+    encryptedText += mapping[character] || character;
   }
-  return transformedText;
+  return encryptedText;
+}
+
+function decrypting(text) {
+  let decryptedText = '';
+
+  for (let i = 0; i < text.length; i++) {
+    const character = text[i];
+    if (character in mapping) {
+      decryptedText += character;
+      i += mapping[character].length - 1;
+    } else {
+      decryptedText += character;
+    }
+  }
+  return decryptedText;
 }
 
 encryptButton.addEventListener('click', (e) => {
   e.preventDefault();
-
   // We encrypted the text of textarea
-  const encryptText = transformText(textArea.value, mapping);
+  const encryptText = encrypting(textArea.value, mapping);
 
   // We create a <p> element to show the encrypted text
   const encryptedTextElement = document.createElement('p');
@@ -67,7 +68,15 @@ encryptButton.addEventListener('click', (e) => {
   // We show the copy button
   copyBtn.classList.remove('hidden-btn');
   copyBtn.classList.add('visible-btn');
-  textArea.value = ''
+  textArea.value = '';
 });
 
-
+decryptButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  let decryptText = decrypting(textArea.value);
+  const decryptedTextElement = document.createElement('p');
+  decryptedTextElement.classList.add('active');
+  decryptedTextElement.textContent = decryptText;
+  outputArea.innerHTML = '';
+  outputArea.appendChild(decryptedTextElement);
+});
